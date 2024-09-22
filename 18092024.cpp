c05fcfa1cc35b0c50dfe7be0d5aaebb52da643ac
Dr34mChas3r <53311619+Dr34mChas3r@users.noreply.github.com>
2024-09-22 17:16:47 +0300

    omg, it`s working :D

--- 18092024.cpp
+++ 18092024.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <math.h>
+#include <cmath>
 using namespace std;
 
 int main()
 {
     for (int number = 1000000000; number <= 2000000000; number++)
     {
+        int root = sqrt(number);
         bool is_prime = true;
-     
-        for (int divider = 2; divider < number; divider++)
+        for (int divider = 2; divider <= root; ++divider)
         {
             if (number % divider == 0)
             {
                 is_prime = false;
-                break;
-            }
-            int root = sqrt(number);
-            if (root % divider == 0) {
-                is_prime = true;
                 break;
             }
         }
