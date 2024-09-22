ce168d5b7bb48fbdf34e9ddbbd2a86de035a6068
Dr34mChas3r <53311619+Dr34mChas3r@users.noreply.github.com>
2024-09-22 16:52:23 +0300

    working? yeees!

--- 18092024.cpp
+++ 18092024.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int main()
@@ -7,12 +7,17 @@ int main()
     for (int number = 1000000000; number <= 2000000000; number++)
     {
         bool is_prime = true;
-
+     
         for (int divider = 2; divider < number; divider++)
         {
             if (number % divider == 0)
             {
                 is_prime = false;
+                break;
+            }
+            int root = sqrt(number);
+            if (root % divider == 0) {
+                is_prime = true;
                 break;
             }
         }
